6b316b819ede4f9e0c412665928e336a2696a005
izauratm <izauratm@gmail.com>
2025-10-20 04:16:12 -0300

    Salvando arquivos locais

--- lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -1,6 +1,5 @@
 package one.digitalinnovation.gof;
 
-import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
